b61335cd1532b6df5e242bf54fe5dc4b7afce66c
kbmakevin <kbma.kevin@gmail.com>
2019-05-05 17:06:36 -0400

    Added stage to Dockerfile to include java jdk11

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const port = 3000
 
 app.get('/', (req,res)=>{
 	console.log('someone accessing \'/\' path')
-	res.send('Hello World!!\nI made a change again :)!\nAnd yet another change!')
+	res.send('Hello World!!\nI made a change :)!\nAnd yet another change!')
 })
 
 app.listen(port,()=>console.log(`Example app listening on port ${port}`))
